334798bf15f8263b8873e04084ca6773437ae980
Mina <1115320273@qq.com>
2025-03-10 23:58:33 +0800

    修复底部tabBar问题

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="content">
-		
+		<view class="topBox">
+			
+		</view>
+		<view class="listBox">
+			
+		</view>
 	</view>
 </template>
 
@@ -17,6 +22,27 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.content {
+	height: 100vh;
+	.topBox {
+		width: 100%;
+		position: relative;
+		z-index: 1;
+		overflow: hidden;
+		padding: 40rpx 20rpx 40rpx;
+		box-sizing: border-box;
+	}
+	.topBox::after {
+		content: "";
+		width: 140%;
+		height: 200px;
+		position: absolute;
+		top: 0;
+		left: -20%;
+		background-color: #00aaff;
+		border-radius: 0 0 50% 50%;
+	}
+}
 
 </style>
