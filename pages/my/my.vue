5e300e7d69c858a3f1fd27809d047626fd7145cd
mina <1115320273@qq.com>
2025-03-13 13:01:45 +0800

    my页面组件

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,10 +1,19 @@
 <template>
 	<view class="content">
 		<view class="topBox">
-			
+			<view class="setBox">
+				<view class="set-left">
+					<uni-icons type="calendar" size="30" color="#fff"></uni-icons>
+					<view class="txt">签到</view>
+				</view>
+				<view class="set-right">
+					<uni-icons type="gear" size="30" color="#fff"></uni-icons>
+					<uni-icons type="chat" size="30" color="#fff"></uni-icons>
+				</view>
+			</view>
 		</view>
 		<view class="listBox">
-			
+
 		</view>
 	</view>
 </template>
@@ -25,6 +34,7 @@
 <style lang="scss" scoped>
 .content {
 	height: 100vh;
+	background-color: #f5f5f5;
 	.topBox {
 		width: 100%;
 		position: relative;
@@ -38,10 +48,27 @@
 		width: 140%;
 		height: 200px;
 		position: absolute;
+		z-index: -1;
 		top: 0;
 		left: -20%;
 		background-color: #00aaff;
 		border-radius: 0 0 50% 50%;
+	}
+	.setBox {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.set-left {
+			width: 18%;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+		.txt {
+			color: #fff;
+			font-size: 30rpx;
+		}
+
 	}
 }
 
--- unpackage/dist/dev/mp-weixin/pages/my/my.js
+++ unpackage/dist/dev/mp-weixin/pages/my/my.js
@@ -6,8 +6,32 @@ const _sfc_main = {
   },
   methods: {}
 };
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  _easycom_uni_icons2();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  _easycom_uni_icons();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.p({
+      type: "calendar",
+      size: "30",
+      color: "#fff"
+    }),
+    b: common_vendor.p({
+      type: "gear",
+      size: "30",
+      color: "#fff"
+    }),
+    c: common_vendor.p({
+      type: "chat",
+      size: "30",
+      color: "#fff"
+    })
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-2f1ef635"]]);
 wx.createPage(MiniProgramPage);
