7c40edf2b788c45939a8524a77073c81defe1e87
mina <1115320273@qq.com>
2025-03-18 13:28:32 +0800

    游玩推荐列表

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -22,7 +22,21 @@
 				</view>
 				<view class="j-con ls">
 					<view class="tit" style="font-size: 34rpx;">游玩推荐</view>
+					<view class="jj tj-list" >
+						<view class="item" v-for="(item, index) in projectList" :key="index">
+							<image :src="item.url" mode="aspectFill"></image>
+							<view class="topFixed">{{ item.tag }}</view>
+							<view class="infos">
+								<view class="tit">{{ item.title }}</view>
+								<view class="desc">
+									<up-icon name="map" color="#9c9c9c" size="16"></up-icon>
+									<text class="text">{{ item.desc }}</text>
+								</view>
+							</view>
+						</view>
+							
 						
+					</view>
 					
 				</view>
 			</view>
@@ -72,6 +86,8 @@
 			padding: 35rpx 30rpx;
 			box-sizing: border-box;
 			border-radius: 0 30rpx;
+
+
 			position: relative;
 			z-index: 9;
 		}
@@ -89,6 +105,58 @@
 					color: #8a8a8a;
 					line-height: 40rpx;
 					margin-bottom: 30rpx;
+				}
+			}
+			.tj-list {
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-between;
+				.item {
+					position: relative;
+					width: 48%;
+					margin-bottom: 20rpx;
+					box-shadow: 1px 2px 3px #e5e5e5e;
+					// border-radius: 0 20rpx;
+					border-top-left-radius: 20rpx;
+					border-top-right-radius: 20rpx;
+					overflow: hidden;
+					.topFixed {
+						position: absolute;
+						top: 0;
+						left: 0;
+						border-top-left-radius: 20rpx;
+						border-bottom-right-radius: 20rpx;
+						background-color: #ffaa7f;
+						color: #fff;
+						text-align: center;
+						font-size: 22rpx;
+						padding: 5rpx 20rpx;
+						box-sizing: border-box;
+					}
+					image {
+						width: 100%;
+						height: 200rpx;
+					}
+					.infos {
+						padding: 10rpx 15rpx;
+						.tit {
+							font-size: 28rpx;
+							font-weight: 700;
+							color: #111;
+							margin-bottom: 15rpx;
+							text-overflow: ellipsis;
+						}
+						.desc {
+							display: flex;
+							justify-content: flex-start;
+							align-items: center;
+							.text {
+								font-size: 26rpx;
+								color: #8a8a8a;
+							}
+						}
+						
+					}
 				}
 			}
 		}
--- unpackage/dist/dev/mp-weixin/pages/detail/detail.js
+++ unpackage/dist/dev/mp-weixin/pages/detail/detail.js
@@ -4,12 +4,14 @@ const api_api = require("../../api/api.js");
 if (!Array) {
   const _easycom_up_navbar2 = common_vendor.resolveComponent("up-navbar");
   const _easycom_up_tag2 = common_vendor.resolveComponent("up-tag");
-  (_easycom_up_navbar2 + _easycom_up_tag2)();
+  const _easycom_up_icon2 = common_vendor.resolveComponent("up-icon");
+  (_easycom_up_navbar2 + _easycom_up_tag2 + _easycom_up_icon2)();
 }
 const _easycom_up_navbar = () => "../../node-modules/uview-plus/components/u-navbar/u-navbar.js";
 const _easycom_up_tag = () => "../../node-modules/uview-plus/components/u-tag/u-tag.js";
+const _easycom_up_icon = () => "../../node-modules/uview-plus/components/u-icon/u-icon.js";
 if (!Math) {
-  (_easycom_up_navbar + _easycom_up_tag)();
+  (_easycom_up_navbar + _easycom_up_tag + _easycom_up_icon)();
 }
 const _sfc_main = {
   __name: "detail",
@@ -21,9 +23,9 @@ const _sfc_main = {
     common_vendor.onLoad((opt) => {
       api_api.detailProject().then((res) => {
         projectList.value = res;
-        common_vendor.index.__f__("log", "at pages/detail/detail.vue:49", projectList.value);
+        common_vendor.index.__f__("log", "at pages/detail/detail.vue:63", projectList.value);
       });
-      common_vendor.index.__f__("log", "at pages/detail/detail.vue:51", JSON.parse(decodeURIComponent(opt.item)));
+      common_vendor.index.__f__("log", "at pages/detail/detail.vue:65", JSON.parse(decodeURIComponent(opt.item)));
       details.dt = JSON.parse(decodeURIComponent(opt.item));
     });
     return (_ctx, _cache) => {
@@ -42,7 +44,22 @@ const _sfc_main = {
           shape: "circle"
         }),
         e: common_vendor.t(details.dt.introduce),
-        f: common_vendor.t(details.dt.times)
+        f: common_vendor.t(details.dt.times),
+        g: common_vendor.f(projectList.value, (item, index, i0) => {
+          return {
+            a: item.url,
+            b: common_vendor.t(item.tag),
+            c: common_vendor.t(item.title),
+            d: "3adf1612-2-" + i0,
+            e: common_vendor.t(item.desc),
+            f: index
+          };
+        }),
+        h: common_vendor.p({
+          name: "map",
+          color: "#9c9c9c",
+          size: "16"
+        })
       };
     };
   }
